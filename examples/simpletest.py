c13121dd477eed8bb71ec33fd184b9e3755d7d89
John Gill <swfiua@gmail.com>
2015-06-25 12:40:29 +0000

    python 3 and add datetime

--- examples/simpletest.py
+++ examples/simpletest.py
@@ -23,8 +23,11 @@
 # Can enable debug output by uncommenting:
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
+from __future__ import print_function
 
 import Adafruit_BMP.BMP085 as BMP085
+
+import datetime
 
 # Default constructor will pick a default I2C bus.
 #
@@ -45,7 +48,9 @@ sensor = BMP085.BMP085()
 # consumption are primarily the differences).  The default mode is STANDARD.
 #sensor = BMP085.BMP085(mode=BMP085.BMP085_ULTRAHIGHRES)
 
-print 'Temp = {0:0.2f} *C'.format(sensor.read_temperature())
-print 'Pressure = {0:0.2f} Pa'.format(sensor.read_pressure())
-print 'Altitude = {0:0.2f} m'.format(sensor.read_altitude())
-print 'Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure())
+
+print('Datetime = {}'.format(datetime.datetime.now()))
+print('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+print('Pressure = {0:0.2f} Pa'.format(sensor.read_pressure()))
+print('Altitude = {0:0.2f} m'.format(sensor.read_altitude()))
+print('Sealevel Pressure = {0:0.2f} Pa'.format(sensor.read_sealevel_pressure()))
